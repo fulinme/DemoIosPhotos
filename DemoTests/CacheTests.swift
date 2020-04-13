f209e153cc7a815b5c3c64ee42e001bd5e3253a1
me <fulin.me@gmail.com>
2020-04-13 10:37:35 +0800

    update

--- DemoTests/CacheTests.swift
+++ DemoTests/CacheTests.swift
@@ -14,12 +14,12 @@ class CacheTests: XCTestCase {
 
     override func setUpWithError() throws {
            // Put setup code here. This method is called before the invocation of each test method in the class.
-           sut = URLSession(configuration: .default)
+           
        }
 
     override func tearDownWithError() throws {
            // Put teardown code here. This method is called after the invocation of each test method in the class.
-           sut = nil
+           
            
     }
 
